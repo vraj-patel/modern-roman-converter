87873729abd8627b7634a6cb331ff0256a4aa88b
vraj-patel <35196999+vraj-patel@users.noreply.github.com>
2018-01-19 22:07:38 -0500

    Add files via upload

---
+++ Vraj - HinduArabic and Roman Numeral Converter/HinduArabic and Roman Numeral Converter/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HinduArabic_and_Roman_Numeral_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputHAOrRomanTextbox = new System.Windows.Forms.TextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.OutputHAOrRomanTextbox = new System.Windows.Forms.TextBox();
+            this.HAToRomanRadioButton = new System.Windows.Forms.RadioButton();
+            this.RomanToHARadioButton = new System.Windows.Forms.RadioButton();
+            this.drawRomanNumberPictureBox = new System.Windows.Forms.PictureBox();
+            this.TextbookViewLabel = new System.Windows.Forms.Label();
+            this.EnterNumberLabel = new System.Windows.Forms.Label();
+            this.ConvertedNumberLabel = new System.Windows.Forms.Label();
+            this.ChooseConversionTypeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawRomanNumberPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputHAOrRomanTextbox
+            // 
+            this.InputHAOrRomanTextbox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.InputHAOrRomanTextbox.Enabled = false;
+            this.InputHAOrRomanTextbox.Location = new System.Drawing.Point(323, 30);
+            this.InputHAOrRomanTextbox.Name = "InputHAOrRomanTextbox";
+            this.InputHAOrRomanTextbox.ShortcutsEnabled = false;
+            this.InputHAOrRomanTextbox.Size = new System.Drawing.Size(201, 20);
+            this.InputHAOrRomanTextbox.TabIndex = 0;
+            this.InputHAOrRomanTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputTextbox_KeyPress);
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(367, 55);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(113, 39);
+            this.ConvertButton.TabIndex = 1;
+            this.ConvertButton.Text = "Convert ";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // OutputHAOrRomanTextbox
+            // 
+            this.OutputHAOrRomanTextbox.Enabled = false;
+            this.OutputHAOrRomanTextbox.Location = new System.Drawing.Point(323, 98);
+            this.OutputHAOrRomanTextbox.Name = "OutputHAOrRomanTextbox";
+            this.OutputHAOrRomanTextbox.ReadOnly = true;
+            this.OutputHAOrRomanTextbox.ShortcutsEnabled = false;
+            this.OutputHAOrRomanTextbox.Size = new System.Drawing.Size(201, 20);
+            this.OutputHAOrRomanTextbox.TabIndex = 2;
+            // 
+            // HAToRomanRadioButton
+            // 
+            this.HAToRomanRadioButton.AutoSize = true;
+            this.HAToRomanRadioButton.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HAToRomanRadioButton.Location = new System.Drawing.Point(16, 52);
+            this.HAToRomanRadioButton.Name = "HAToRomanRadioButton";
+            this.HAToRomanRadioButton.Size = new System.Drawing.Size(163, 25);
+            this.HAToRomanRadioButton.TabIndex = 3;
+            this.HAToRomanRadioButton.TabStop = true;
+            this.HAToRomanRadioButton.Text = "Hindu Arabic To Roman";
+            this.HAToRomanRadioButton.UseVisualStyleBackColor = true;
+            this.HAToRomanRadioButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.HAToRomanRadioButton_MouseClick);
+            // 
+            // RomanToHARadioButton
+            // 
+            this.RomanToHARadioButton.AutoSize = true;
+            this.RomanToHARadioButton.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RomanToHARadioButton.Location = new System.Drawing.Point(16, 83);
+            this.RomanToHARadioButton.Name = "RomanToHARadioButton";
+            this.RomanToHARadioButton.Size = new System.Drawing.Size(163, 25);
+            this.RomanToHARadioButton.TabIndex = 4;
+            this.RomanToHARadioButton.TabStop = true;
+            this.RomanToHARadioButton.Text = "Roman To Hindu Arabic";
+            this.RomanToHARadioButton.UseVisualStyleBackColor = true;
+            this.RomanToHARadioButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RomanToHARadioButton_MouseClick);
+            // 
+            // drawRomanNumberPictureBox
+            // 
+            this.drawRomanNumberPictureBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.drawRomanNumberPictureBox.Location = new System.Drawing.Point(12, 147);
+            this.drawRomanNumberPictureBox.Name = "drawRomanNumberPictureBox";
+            this.drawRomanNumberPictureBox.Size = new System.Drawing.Size(600, 222);
+            this.drawRomanNumberPictureBox.TabIndex = 5;
+            this.drawRomanNumberPictureBox.TabStop = false;
+            // 
+            // TextbookViewLabel
+            // 
+            this.TextbookViewLabel.AutoSize = true;
+            this.TextbookViewLabel.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextbookViewLabel.Location = new System.Drawing.Point(12, 120);
+            this.TextbookViewLabel.Name = "TextbookViewLabel";
+            this.TextbookViewLabel.Size = new System.Drawing.Size(109, 19);
+            this.TextbookViewLabel.TabIndex = 6;
+            this.TextbookViewLabel.Text = "Textbook View";
+            // 
+            // EnterNumberLabel
+            // 
+            this.EnterNumberLabel.AutoSize = true;
+            this.EnterNumberLabel.Location = new System.Drawing.Point(217, 33);
+            this.EnterNumberLabel.Name = "EnterNumberLabel";
+            this.EnterNumberLabel.Size = new System.Drawing.Size(100, 13);
+            this.EnterNumberLabel.TabIndex = 7;
+            this.EnterNumberLabel.Text = "Enter Your Number:";
+            // 
+            // ConvertedNumberLabel
+            // 
+            this.ConvertedNumberLabel.AutoSize = true;
+            this.ConvertedNumberLabel.Location = new System.Drawing.Point(217, 100);
+            this.ConvertedNumberLabel.Name = "ConvertedNumberLabel";
+            this.ConvertedNumberLabel.Size = new System.Drawing.Size(99, 13);
+            this.ConvertedNumberLabel.TabIndex = 8;
+            this.ConvertedNumberLabel.Text = "Converted Number:";
+            // 
+            // ChooseConversionTypeLabel
+            // 
+            this.ChooseConversionTypeLabel.AutoSize = true;
+            this.ChooseConversionTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChooseConversionTypeLabel.ForeColor = System.Drawing.Color.Red;
+            this.ChooseConversionTypeLabel.Location = new System.Drawing.Point(21, 24);
+            this.ChooseConversionTypeLabel.Name = "ChooseConversionTypeLabel";
+            this.ChooseConversionTypeLabel.Size = new System.Drawing.Size(152, 13);
+            this.ChooseConversionTypeLabel.TabIndex = 9;
+            this.ChooseConversionTypeLabel.Text = "Choose Conversion Type:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Input \"#\" For a Bar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(638, 381);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChooseConversionTypeLabel);
+            this.Controls.Add(this.ConvertedNumberLabel);
+            this.Controls.Add(this.EnterNumberLabel);
+            this.Controls.Add(this.TextbookViewLabel);
+            this.Controls.Add(this.drawRomanNumberPictureBox);
+            this.Controls.Add(this.RomanToHARadioButton);
+            this.Controls.Add(this.HAToRomanRadioButton);
+            this.Controls.Add(this.OutputHAOrRomanTextbox);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.InputHAOrRomanTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawRomanNumberPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputHAOrRomanTextbox;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.TextBox OutputHAOrRomanTextbox;
+        private System.Windows.Forms.RadioButton HAToRomanRadioButton;
+        private System.Windows.Forms.RadioButton RomanToHARadioButton;
+        private System.Windows.Forms.PictureBox drawRomanNumberPictureBox;
+        private System.Windows.Forms.Label TextbookViewLabel;
+        private System.Windows.Forms.Label EnterNumberLabel;
+        private System.Windows.Forms.Label ConvertedNumberLabel;
+        private System.Windows.Forms.Label ChooseConversionTypeLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Vraj - HinduArabic and Roman Numeral Converter/HinduArabic and Roman Numeral Converter/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+
+namespace HinduArabic_and_Roman_Numeral_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // These arrays are created within the createArrays method and are used within other methods
+        string[] newRomanSymbols;
+        BigInteger[] newRomanValues;
+
+        private void HAToRomanRadioButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            InputHAOrRomanTextbox.Enabled = true;
+            InputHAOrRomanTextbox.Clear();
+            OutputHAOrRomanTextbox.Enabled = true;
+            ChooseConversionTypeLabel.ForeColor = Color.Black;
+        }
+
+        private void RomanToHARadioButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            InputHAOrRomanTextbox.Enabled = true;
+            InputHAOrRomanTextbox.Clear();
+            OutputHAOrRomanTextbox.Enabled = true;
+            ChooseConversionTypeLabel.ForeColor = Color.Black;
+        }
+
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            Bitmap drawRomanNumberBtmp = null;
+
+            if (HAToRomanRadioButton.Checked && InputHAOrRomanTextbox.Text.Length >= 1)
+            {
+                // creates the new arrays depending on the size hinduarabic number entered
+                createArrays(BigInteger.Parse(InputHAOrRomanTextbox.Text));
+
+                if (newRomanSymbols.Length > 14)
+                {
+                    drawRomanNumberBtmp = new Bitmap(newRomanSymbols.Length * 20, 222);
+                    drawRomanNumberPictureBox.Width = newRomanSymbols.Length * 20;
+                }
+
+                else
+                {
+                    drawRomanNumberPictureBox.Width = 600;
+                    drawRomanNumberBtmp = new Bitmap(600, 222);
+                }
+
+                OutputHAOrRomanTextbox.Text = hinduArabicTORoman(BigInteger.Parse(InputHAOrRomanTextbox.Text), drawRomanNumberBtmp);
+            }
+
+            else if (InputHAOrRomanTextbox.Text.Length == 0 && (HAToRomanRadioButton.Checked || RomanToHARadioButton.Checked))
+                OutputHAOrRomanTextbox.Text = "Please Enter a Number To Convert";
+
+            else if (RomanToHARadioButton.Checked && InputHAOrRomanTextbox.Text.Substring(0, 1) == "#")
+                OutputHAOrRomanTextbox.Text = "The bar alone has no value (0*1000)";
+
+            else if (RomanToHARadioButton.Checked && InputHAOrRomanTextbox.Text.Length >= 1)
+            {
+                createArrays(BigInteger.Parse("1"));
+
+                string storeConversion = romanToHinduArabic(InputHAOrRomanTextbox.Text);
+
+                // the original arrays need to be overwritten since the hinduArabicToRoman method is called bellow
+                createArrays(BigInteger.Parse(storeConversion));
+
+                // the input (storeConversion) is converted into a bigint and a roman output is produced  
+                string storeOtherConversion = hinduArabicTORoman(BigInteger.Parse(storeConversion), drawRomanNumberBtmp);
+
+                if (InputHAOrRomanTextbox.Text.Equals(storeOtherConversion))
+                    OutputHAOrRomanTextbox.Text = storeConversion;
+
+                else
+                    OutputHAOrRomanTextbox.Text = "Invalid Roman Number";
+
+            }
+
+            else if ((HAToRomanRadioButton.Checked && RomanToHARadioButton.Checked) == false)
+                OutputHAOrRomanTextbox.Text = "Please Select Conversion Type";
+
+        }
+
+
+
+        // This method creates or adds elements to the public arrays by augmenting the initial arrays 
+        private void createArrays(BigInteger number)
+        {
+            int highestNumOfBars = 0;
+
+            BigInteger numberRange = 4000;
+
+            BigInteger[] initialRomanValues = { 1, 5, 10, 50, 100, 500, 1000 };
+            string[] initialRomanSymbols = { "I", "V", "X", "L", "C", "D", "M" };
+
+            while (numberRange <= number)
+            {
+                highestNumOfBars++;
+                numberRange *= 1000;
+            }
+
+            // calculates the size of new arrays 
+            newRomanSymbols = new string[6 * highestNumOfBars + 7];
+            newRomanValues = new BigInteger[newRomanSymbols.Length];
+
+            for (int x = 0; x < initialRomanSymbols.Length; x++)
+            {
+                newRomanSymbols[x] = initialRomanSymbols[x];
+                newRomanValues[x] = initialRomanValues[x];
+            }
+
+            // adds roman numerals with '#' (bars), excluding the products of 'I' and 1000^n (since they can be written as 'M'* 1000^n-1) 
+            for (int x = 7; x < newRomanSymbols.Length; x++)
+            {
+                if (newRomanSymbols[x - 7] == "I")
+                {
+                    newRomanSymbols[x] = newRomanSymbols[x - 6] + "#";
+                    newRomanValues[x] = newRomanValues[x - 6] * 1000;
+                }
+                else
+                {
+                    newRomanSymbols[x] = newRomanSymbols[x - 6] + "#";
+                    newRomanValues[x] = newRomanValues[x - 6] * 1000;
+                }
+            }
+
+        }
+
+
+
+
+        // The function of this method is to draw the roman numerals 
+        private void drawRomanNumerals(string symbol, float posX, int numberOfBars, Bitmap drawRomanNumberBtmp)
+        {
+
+            SolidBrush brush = new SolidBrush(Color.Purple);
+
+            Graphics graphic = Graphics.FromImage(drawRomanNumberBtmp);
+            Font font = new Font("Arial", 12);
+            Font font2 = new Font("Arial", 10);
+
+            graphic.DrawString(symbol, font, brush, posX, 175);
+
+            Pen drawBarsPen = new Pen(Color.Purple);
+
+            float posYForBars = 175;
+            for (int counter = 1; counter <= numberOfBars; counter++)
+            {
+                posYForBars -= 5;
+                graphic.DrawLine(drawBarsPen, posX + 4, posYForBars, posX + 12, posYForBars);
+            }
+            if (numberOfBars > 0)
+                graphic.DrawString(numberOfBars.ToString(), font2, brush, posX + 1, 195);
+
+        }
+
+
+
+
+        // Converts HinduArabic Numbers to Roman and uses the drawRomanNumerals method to create an accurate visual 
+        private string hinduArabicTORoman(BigInteger hinduArabicNumber, Bitmap drawRomanNumberBtmp)
+        {
+            BigInteger remainder = hinduArabicNumber;
+            BigInteger quotient = 0;
+            string romanNumber = "";
+            string symbol = "";
+            int numberOfBars = 0;
+            float posX = 15;
+
+            for (int x = newRomanValues.Length - 1; x >= 0; x--)
+            {
+                symbol = "";
+                numberOfBars = 0;
+                quotient = remainder / newRomanValues[x];
+
+                /* if romanValue[x] does not divide into the remainder atleast once, the quotient using intermediate symbols 
+                 * (ex: IV, IX, ...) is checked. Since intermediate values consist of the original symbol along with another 
+                 * symbol (which is a multiple of 10 or 0.1 apart) , only 2 other successive symbols can be allowed
+                 * */
+                if (quotient == 0 && x >= 2)
+                {
+                    for (int y = 1; y <= 2; y++)
+                    {
+                        quotient = remainder / (newRomanValues[x] - newRomanValues[x - y]);
+                        if (quotient == 1 && newRomanValues[x] - newRomanValues[x - y] != newRomanValues[x - y])
+                        {
+                            romanNumber += newRomanSymbols[x - y] + newRomanSymbols[x];
+                            symbol += newRomanSymbols[x - y] + newRomanSymbols[x];
+
+                            remainder -= (newRomanValues[x] - newRomanValues[x - y]);
+                        }
+                    }
+                }
+
+                // Same reasoning as mentioned above, but if x == 1, there is only 1 successive symbol left to check within the array 
+                else if (quotient == 0 && x == 1)
+                {
+                    quotient = remainder / (newRomanValues[x] - newRomanValues[x - 1]);
+                    if (quotient == 1 && newRomanValues[x] - newRomanValues[x - 1] != newRomanValues[x - 1])
+                    {
+                        romanNumber += newRomanSymbols[x - 1] + newRomanSymbols[x];
+                        symbol += newRomanSymbols[x - 1] + newRomanSymbols[x];
+
+                        remainder -= (newRomanValues[x] - newRomanValues[x - 1]);
+                    }
+                }
+
+                // Within this case, single (original) symbols are added to the romanNumber string, instead of intermediates
+                else
+                {
+                    for (int z = 1; z <= quotient; z++)
+                    {
+                        romanNumber += newRomanSymbols[x];
+                        symbol += newRomanSymbols[x];
+
+                        remainder -= newRomanValues[x];
+                    }
+                    // This case is used to print out the final intermediate symbol (if it divides into the remainder)
+                    if (x >= 2)
+                    {
+                        if (newRomanValues[x] - newRomanValues[x - 2] <= remainder)
+                        {
+                            romanNumber += newRomanSymbols[x - 2] + newRomanSymbols[x];
+                            symbol += newRomanSymbols[x - 2] + newRomanSymbols[x];
+
+                            remainder -= (newRomanValues[x] - newRomanValues[x - 2]);
+                        }
+                    }
+                }
+
+                // This case uses the drawRomanNumerals method to create the visual within a bitmap
+                if (HAToRomanRadioButton.Checked)
+                {
+                    for (int counter = 0; counter <= symbol.Length - 1; counter++)
+                    {
+                        posX += 15;
+
+                        numberOfBars = 0;
+
+                        while (counter <= symbol.Length - 2 && symbol.Substring(counter + 1, 1) == "#")
+                        {
+                            numberOfBars++;
+                            counter++;
+                        }
+
+                        drawRomanNumerals(symbol.Substring(counter - numberOfBars, 1), posX, numberOfBars, drawRomanNumberBtmp);
+
+                    }
+                }
+
+
+            }
+
+            if (HAToRomanRadioButton.Checked)
+                drawRomanNumberPictureBox.Image = drawRomanNumberBtmp;
+
+            return romanNumber;
+        }
+
+
+
+        // Checks validity of Roman number entered and converts it to Hindu-Arabic
+        private string romanToHinduArabic(string romanNumber)
+        {
+            BigInteger hinduArabicNumber = 0;
+            BigInteger currentSymbolValue = 0;
+            BigInteger nextSymbolValue = 0;
+            int numberOfBars = 0;
+            int counterX = 0;
+
+            if (romanNumber.Substring(0, 1) == "#")
+                return "A Bar Alone Has No Value (0*1000)";
+
+
+            while (counterX <= romanNumber.Length - 1)
+            {
+                numberOfBars = 0;
+                currentSymbolValue = newRomanValues[Array.IndexOf(newRomanSymbols, romanNumber.Substring(counterX, 1))];
+
+                if (romanNumber.Length == 1)
+                {
+                    hinduArabicNumber += currentSymbolValue;
+                    break;
+                }
+
+                else if (romanNumber.Replace("#", "").Length == 1)
+                {
+                    while (counterX <= romanNumber.Length - 1)
+                    {
+                        if (romanNumber.Substring(counterX, 1) == "#")
+                        {
+                            numberOfBars++;
+                            currentSymbolValue *= 1000;
+                        }
+                        counterX++;
+                    }
+
+                    hinduArabicNumber += currentSymbolValue;
+                    break;
+                }
+
+                while (counterX <= romanNumber.Length - 2 && romanNumber.Substring(counterX + 1, 1) == "#")
+                {
+                    counterX++;
+                    numberOfBars++;
+                    currentSymbolValue *= 1000;
+                }
+
+                counterX++;
+                numberOfBars = 0;
+
+                if (counterX > romanNumber.Length - 1)
+                    nextSymbolValue = 0;
+                else
+                    nextSymbolValue = newRomanValues[Array.IndexOf(newRomanSymbols, romanNumber.Substring(counterX, 1))];
+
+                while (counterX <= romanNumber.Length - 2 && romanNumber.Substring(counterX + 1, 1) == "#")
+                {
+                    counterX++;
+                    numberOfBars++;
+                    nextSymbolValue *= 1000;
+                }
+
+                counterX -= numberOfBars;
+
+                if (currentSymbolValue < nextSymbolValue)
+                {
+                    hinduArabicNumber -= currentSymbolValue;
+                }
+                else if (currentSymbolValue > nextSymbolValue)
+                {
+                    hinduArabicNumber += currentSymbolValue;
+                }
+                else if (currentSymbolValue == nextSymbolValue)
+                {
+                    hinduArabicNumber += currentSymbolValue;
+                }
+            }
+
+            return hinduArabicNumber.ToString();
+        }
+
+
+
+        private void InputTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (RomanToHARadioButton.Checked)
+            {
+                if (Char.IsControl(e.KeyChar) || e.KeyChar == 'I' || e.KeyChar == 'V' || e.KeyChar == 'X' || e.KeyChar == 'L' || e.KeyChar == 'C' || e.KeyChar == 'D' || e.KeyChar == 'M' || e.KeyChar == '#')
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+
+            else if (HAToRomanRadioButton.Checked)
+            {
+                if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+
+        }
+
+
+    }
+}
+
+
+
+
